e7754abda696718f031e4fe88e2a0338653d5d51
chxxyx <codud0826@naver.com>
2023-08-24 02:52:11 +0900

    [Add] Sequelize 관계선언  추가

--- src/database/index.js
+++ src/database/index.js
@@ -47,4 +47,35 @@ db.Sequelize = Sequelize;
 //     }
 // });
 
+// User 
+db.User.hasOne(db.Password, { foreignKey: 'user_id' });
+db.User.hasMany(db.Preference, { foreignKey: 'user_id' });
+db.User.hasMany(db.UserKeyword, { foreignKey: 'user_id' });
+
+db.User.hasMany(db.Block, { foreignKey: 'user_id' });
+db.Block.belongsTo(db.User, { foreignKey: 'block_user_id' });
+
+db.User.hasMany(db.Profile, { foreignKey: 'user_id' });
+db.User.hasMany(db.Neighbor, { foreignKey: 'user_id' });
+db.User.hasMany(db.SoicalLogin, { foreignKey: 'user_id' });
+db.User.hasMany(db.Post, { foreignKey: 'user_id' });
+db.User.hasMany(db.Category, { foreignKey: 'user_id' });
+db.User.hasMany(db.comment, { foreignKey: 'user_id' });
+db.User.hasMany(db.Bookmark, { foreignKey: 'user_id' });
+db.User.hasMany(db.Like, { foreignKey: 'user_id' });
+
+// UserKeyword
+db.UserKeyword.hasMany(db.Keyword, { foreignKey: 'user_id'});
+
+// Category
+db.Category.hasMany(db.Post, { foreignKey: 'catagory_id'});
+
+// Post
+db.Post.hasMany(db.Image, { foreignKey: 'post_id'});
+db.Post.hasMany(db.Like, { foreignKey: 'post_id'});
+db.Post.hasMany(db.comment, { foreignKey: 'post_id'});
+db.Post.hasMany(db.Bookmark, { foreignKey: 'post_id'});
+
+db.comment.hasMany(db.comment, { foreignKey: 'parent_id'});
+
 export default db;
