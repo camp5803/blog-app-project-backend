70f9108ec69147b2aa56dc64cb2870d655eeee4e
Jiyong Jung <hickerd@sch.ac.kr>
2023-08-26 05:44:31 +0900

    [Refactor] 코드 리팩터링

--- src/controller/post.controller.js
+++ src/controller/post.controller.js
@@ -1,21 +1,15 @@
 import { asyncWrapper } from '@/common/index';
 import { postService } from '@/service/index';
+import { StatusCodes } from 'http-status-codes';
 
 export const createPost = asyncWrapper(async (req, res) => {
-        try {
-            const { user_id, title, content, category_id, img} = req.body;
-            const postsInput = {
-                user_id: user_id,
-                title: title,
-                content: content,
-                category_id: category_id,
-                img: img,
-            }
-            console.log(postsInput);
-            const post = await postService.createPost(postsInput);
-            console.log('controller', post);
-            res.status(201).send({ data: post });
-        } catch (error) {
-            res.status(500).send(error);
-        }
-})
+    const post = await postService.createPost({
+        user_id: req.body.user_id,
+        title: req.body.title,
+        content: req.body.content,
+        category_id: req.body.category_id,
+        img: req.body.img
+    });
+
+    res.status(StatusCodes.CREATED).end();
+});
