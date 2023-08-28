5baee5ff800bfc043f386352d79c5e3d9af606d1
Jiyong Jung <hickerd@sch.ac.kr>
2023-08-28 12:32:11 +0900

    [Fix] bcrypt 구문 수정 callback → Sync

--- src/utils/security.js
+++ src/utils/security.js
@@ -1,13 +1,13 @@
 import bcrypt from 'bcrypt';
 
 export const createPassword = (plainText) => {
-    const saltRounds = 10;
-
-    bcrypt.getSalt(saltRounds, (err, salt) => {
-        if (err) throw new Error(err);
-        bcrypt.hash(plainText, salt, (err, cipherText) => {
-            if (err) throw new Error(err);
-            return cipherText;
-        })
-    })
+    try {
+        const saltRounds = 10;
+        return bcrypt.hashSync(plainText, bcrypt.genSaltSync(saltRounds));  
+    } catch (err) {
+        return {
+            error: true,
+            message: err
+        }
+    }
 }
