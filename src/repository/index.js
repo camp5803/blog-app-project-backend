f1d618be2d32c4d069e0c6a0bc852fbf1ec3b1e6
Jiyong Jung <hickerd@sch.ac.kr>
2023-08-29 16:08:46 +0900

    [Refactor] password 레포지토리 분리

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,4 +1,5 @@
 export { userRepository } from '@/repository/user.repository';
 export { authRepository } from '@/repository/auth.repository';
+export { passwordRepository } from '@/repository/password.repository';
 export { socialLoginRepository } from '@/repository/social.login.repository';
 export { createPost } from '.@/repository/post.repository';
