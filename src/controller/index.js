9b60c1475ceec370efed4072d008bbd71412c3ae
chxxyx <codud0826@naver.com>
2023-08-30 16:36:33 +0900

    [Fix] develop merge conflict 코드 수정

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,6 +1,3 @@
-export { createUser, getUser, getAllUsers, updateUser, deleteUser } from './user.controller';
-export { createAuth, updateAuth } from './auth.controller';
-export { createPost, updatePost, deletePost, getByPostDetail, getPostsByPage } from './post.controller';
 export { createLocalUser, deleteUser } from './user.controller';
 export { createAuth, reissueAccessToken, socialCallbackHandler, createSocialAuth } from './auth.controller';
-export { createPost, updatePost, deletePost, getByPostDetail, getByAllList } from './post.controller';
+export { createPost, updatePost, deletePost, getByPostDetail, getPostsByPage } from './post.controller';
