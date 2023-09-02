beaaaa403e63a720b7d79d1ae02c1885a35d1151
Jiyong Jung <hickerd@sch.ac.kr>
2023-09-02 17:25:20 +0900

    [Fix] 게시판 관련 인증 미들웨어 추가

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { createPost, updatePost, deletePost, getByPostDetail, getByAllList } from '@/controller/index.js';
+import { createPost, updatePost, deletePost, getByPostDetail, getByAllList } from '@/controller';
 const router = express.Router();
+import { isAuthenticated, isAuthorized } from "@/middleware";
 
-router.route('/post').post(createPost);
-router.route('/post/:id').patch(updatePost);
-router.route('/post/:id').delete(deletePost);
-router.route('/post').get(getByPostDetail);
+router.route('/post').post(isAuthorized, createPost); // POST /posts로 변경하세요
+router.route('/post/:id')
+    .delete(isAuthorized, deletePost)
+    .patch(isAuthorized, updatePost);
+router.route('/post').get(getByPostDetail); // GET /posts/:id로 Detail 조회시키세요
 router.route('/posts').get(getByAllList);
 
 export default router;
