3178b2d04583da36208cb2792fff3bbf04bd5440
Jiyong Jung <hickerd@sch.ac.kr>
2023-09-04 13:35:42 +0900

    [Feat] 추가된 기능 index.js에 반영

--- src/repository/index.js
+++ src/repository/index.js
@@ -2,4 +2,5 @@ export { userRepository } from '@/repository/user.repository';
 export { authRepository } from '@/repository/auth.repository';
 export { passwordRepository } from '@/repository/password.repository';
 export { socialLoginRepository } from '@/repository/social.login.repository';
+export { profileRepository } from '@/repository/profile.repository';
 export { createPost, updatePost, deletePost, getByPostDetail, getPostsByPage } from './post.repository';
