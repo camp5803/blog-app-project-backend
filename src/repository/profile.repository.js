13911336b0621425a45d5decb15bd2c93e74d33d
Jiyong Jung <hickerd@sch.ac.kr>
2023-09-04 20:23:22 +0900

    [Fix] 미들웨어 수정 반영

--- src/repository/profile.repository.js
+++ src/repository/profile.repository.js
@@ -1,7 +1,12 @@
 import db from '@/database';
+import { verifyToken } from "@/utils";
 const { Profile } = db;
 
 export const profileRepository = {
+    findUserIdByToken: (token) => {
+        const result = verifyToken(token);
+        return result.user_id;
+    },
     findByUserId: async (user_id) => {
         return await Profile.findOne({ where: { user_id } });
     },
