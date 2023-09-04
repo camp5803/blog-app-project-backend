9b9e16745496834998e1c7f199095bc5261550f5
Jiyong Jung <hickerd@sch.ac.kr>
2023-09-04 13:40:03 +0900

    [Feat] 추가된 유저 기능 Router 반영

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import { createLocalUser, deleteUser } from '@/controller/index';
+import { createLocalUser, deleteUser, getProfileById, updateUser, updateProfileImage, validateEmail } from '@/controller';
+import { upload } from '@/utils';
 
 const router = express.Router();
 
 router.route('/users')
     .post(createLocalUser)
     .delete(deleteUser);
+
+
+router.patch('/users/name/:id', updateUser);
+router.patch('/users/image/:id', upload.single('image'), updateProfileImage);
+router.get('/users/email', validateEmail)
+router.get('/users/:id', getProfileById)
 
 export default router;
