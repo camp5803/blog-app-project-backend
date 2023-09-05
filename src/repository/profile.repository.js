43e545833c916aed5a71debab7a7136f7632d9a5
Jiyong Jung <hickerd@sch.ac.kr>
2023-09-05 18:35:10 +0900

    [Fix] API 응답 수정 → 클라이언트 수정 반영

--- src/repository/profile.repository.js
+++ src/repository/profile.repository.js
@@ -1,11 +1,18 @@
 import db from '@/database';
 import { verifyToken } from "@/utils";
-const { Profile } = db;
+const { Profile, Preference } = db;
 
 export const profileRepository = {
     findUserIdByToken: (token) => {
         const result = verifyToken(token);
         return result.user_id;
+    },
+    findUserInformationById: async (user_id) => {
+        return await Profile.findOne({
+            where: { user_id },
+            attributes: ['nickname', 'image_url'],
+            include: [ { model: Preference, attributes: ['darkmode_status'] }]
+        });
     },
     findByUserId: async (user_id) => {
         return await Profile.findOne({ where: { user_id } });
