4159abd255b0a9976a2c15ee7147d66e7702a6b2
Jiyong Jung <hickerd@sch.ac.kr>
2023-09-06 18:07:31 +0900

    [Refactor] 로컬 로그인 재구현

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -5,29 +5,36 @@ export const userService = {
         try {
             const user = await userRepository.findByEmail(email);
             if (user) {
-                return {
-                    message: "Email Already exists."
-                }
+                return { message: "Email Already exists." }
             }
             return { OK: true };
         } catch (error) {
             return { message: error }
         }
     },
-    createUser: async (data) => { // data의 key는 email, login_type, nickname, password
-        const user = await userRepository.findByEmail(data.email);
-        if (user) {
-            return {
-                error: true,
-                message: "[Signup Error#1] Email Already exists."
-            }
-        }
-
-        return await userRepository.createUser(data);
-    },
-    updateUser: async (user_id, data) => {
+    getUserInformation: async (accessToken) => {
         try {
-            const user = await profileRepository.findByUserId(user_id);
+            const userId = profileRepository.findUserIdByToken(accessToken);
+            const userData = await profileRepository.findUserInformationById(userId);
+            return userData.dataValues;
+        } catch (error) {
+            return { message: error.message };
+        }
+    },
+    createUser: async (data) => {
+        try {
+            const user = await userRepository.findByEmail(data.email);
+            if (user) {
+                return { message: "[Signup Error#1] Email Already exists." }
+            }
+            return await userRepository.createUser(data);
+        } catch (error) {
+            return { message: error.message }
+        }
+    },
+    updateUser: async (userId, data) => {
+        try {
+            const user = await profileRepository.findByUserId(userId);
             const userData = user.dataValues;
 
             Object.keys(userData).forEach(key => {
@@ -36,7 +43,7 @@ export const userService = {
                 }
             });
 
-            return await profileRepository.updateProfile(user_id, userData);
+            return await profileRepository.updateProfile(userId, userData);
         } catch (error) {
             return { message: error }
         }
