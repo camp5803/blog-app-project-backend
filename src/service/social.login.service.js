55ce36259ae30dd1e3d7f698e4f8c8f5ef8c3815
Jiyong Jung <hickerd@sch.ac.kr>
2023-09-06 20:35:26 +0900

    [Fix] 소셜 로그인 에러 수정 및 이전 코드 삭제

--- src/service/social.login.service.js
+++ src/service/social.login.service.js
@@ -41,6 +41,7 @@ const Options = [null, { // 1번 index : KAKAO
 export const socialLoginService = {
     login: async (type, code, uri) => { // type에 .toUpperCase 사용해서 전달하기
         let profile, email;
+        type = socialCode[type];
         try {
             const socialToken = await axios.post(Options[type].requestToken, null, {
                 params: {
@@ -65,7 +66,7 @@ export const socialLoginService = {
                 profile = await profileRepository.findUserInformationById(newUser.user_id);
                 email = newUser.dataValues.email;
             }
-            const userToken = await createToken(profile.user_id);
+            const userToken = await createToken(user.user_id);
             return {
                 token: userToken,
                 profile : {
@@ -78,139 +79,4 @@ export const socialLoginService = {
             return { message: error.message }
         }
     },
-    kakaoLoginService: async (code, uri) => {
-        try {
-            const token = await axios.post('https://kauth.kakao.com/oauth/token', null, {
-                params: {
-                    code,
-                    client_id: kakaoOptions.clientID,
-                    client_secret: kakaoOptions.clientSecret,
-                    redirect_url: uri,
-                    grant_type: 'authorization_code'
-                }
-            });
-            const kakaoUser = await axios.get('https://kapi.kakao.com/v2/user/me', {
-                headers: {
-                    'Authorization': `Bearer ${token.data.access_token}`
-                }
-            });
-            if (!kakaoUser) {
-                return { error: "" }
-            }
-            const user = await socialLoginRepository.findBySocialId(kakaoUser.data.id);
-            if (!user) {
-                const newUser = await socialLoginRepository.createSocialUser({
-                    email: kakaoUser.data.kakao_account.email || null,
-                    code: socialCode.KAKAO,
-                    name: kakaoUser.data.kakao_account.profile.nickname,
-                    type: "kakao",
-                    id: kakaoUser.data.id,
-                    image_url: kakaoUser.data.kakao_account.profile.profile_image_url
-                });
-                const createdToken = await createToken(newUser.user_id);
-                return {
-                    accessToken: createdToken.accessToken,
-                    refreshToken: createdToken.refreshToken,
-                    email: !(newUser.dataValues.email),
-                }
-            }
-            const createdToken = await createToken(user.user_id);
-            return {
-                accessToken: createdToken.accessToken,
-                refreshToken: createdToken.refreshToken,
-                email: !(user.dataValues.email),
-            }
-        } catch (error) {
-            return { error };
-        }
-    },
-    githubLoginService: async (code, uri) => {
-        try {
-            const token = await axios.post('https://github.com/login/oauth/access_token', null, {
-                params: {
-                    code,
-                    client_id: githubOptions.clientID,
-                    client_secret: githubOptions.clientSecret,
-                    redirect_url: uri,
-                }
-            });
-            const tokenData = new URLSearchParams(token.data);
-            const githubUser = await axios.get('https://api.github.com/user', {
-                headers: {
-                    'Authorization': `Bearer ${tokenData.get('access_token')}`
-                }
-            });
-            if (!githubUser) {
-                return { error: "" }
-            }
-            const user = await socialLoginRepository.findBySocialId(githubUser.data.id);
-            if (!user) {
-                const newUser = await socialLoginRepository.createSocialUser({
-                    email: githubUser.data.email || null,
-                    code: socialCode.GITHUB,
-                    type: "github",
-                    name: githubUser.data.name,
-                    id: githubUser.data.id,
-                    image_url: githubUser.data.avatar_url
-                });
-                const createdToken = await createToken(newUser.user_id);
-                return {
-                    accessToken: createdToken.accessToken,
-                    refreshToken: createdToken.refreshToken,
-                    email: !(newUser.dataValues.email),
-                }
-            }
-            const createdToken = await createToken(user.user_id);
-            return {
-                accessToken: createdToken.accessToken,
-                refreshToken: createdToken.refreshToken,
-                email: !(user.dataValues.email),
-            }
-        } catch (error) {
-            return { error };
-        }
-    },
-    googleLoginService: async (code, uri) => {
-        try {
-            const token = await axios.post('https://oauth2.googleapis.com/token', {}, {
-                params: {
-                    code,
-                    client_id: googleOptions.clientID,
-                    client_secret: googleOptions.clientSecret,
-                    redirect_uri: uri,
-                    grant_type: 'authorization_code'
-                }
-            });
-            const googleUser = await axios.get('https://www.googleapis.com/userinfo/v2/me', {
-                headers: {
-                    'Authorization': `Bearer ${token.data.access_token}`
-                }
-            });        
-            const user = await socialLoginRepository.findBySocialId(googleUser.data.id); // 여기서 User레포에 요청
-            if (!user) {
-                const newUser = await socialLoginRepository.createSocialUser({
-                    email: googleUser.data.email || null,
-                    code: socialCode.GOOGLE,
-                    type: "google",
-                    name: googleUser.data.name,
-                    id: googleUser.data.id,
-                    image_url: googleUser.data.picture
-                });
-                const createdToken = await createToken(newUser.user_id);
-                return {
-                    accessToken: createdToken.accessToken,
-                    refreshToken: createdToken.refreshToken,
-                    email: !(newUser.dataValues.email),
-                }
-            }
-            const createdToken = await createToken(user.user_id);
-            return {
-                accessToken: createdToken.accessToken,
-                refreshToken: createdToken.refreshToken,
-                email: !(user.dataValues.email),
-            }
-        } catch (error) {
-            return { error };
-        }
-    }
 }
