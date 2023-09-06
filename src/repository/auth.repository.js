437403e151101b4bf12ee853e98d2b9790405867
Jiyong Jung <hickerd@sch.ac.kr>
2023-09-06 17:58:31 +0900

    [Refactor] 필요 없는 코드 정리

--- src/repository/auth.repository.js
+++
@@ -1,7 +0,0 @@
-import { redisCli as redisClient } from '@/utils/index'
-
-export const authRepository = {
-    getRefreshToken: async (userId) => {
-        return await redisClient.get(userId.toString());
-    }
-}
