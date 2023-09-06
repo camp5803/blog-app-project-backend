6deffa5ea5dc73d5a4def1d298d73a097e3cbd0a
Jiyong Jung <hickerd@sch.ac.kr>
2023-09-06 17:57:10 +0900

    [Refactor] Passport 잔재 제거, 소셜 로그인 재구현

--- src/repository/social.login.repository.js
+++ src/repository/social.login.repository.js
@@ -1,36 +1,114 @@
 import db from '@/database/index';
 const { User, Preference, Profile, SocialLogin, sequelize } = db;
 
+const socialCode = {
+    KAKAO: 1,
+    GITHUB: 2,
+    GOOGLE: 3
+}
+
+const typeFormatters = {
+    email: {
+        KAKAO: (data) => data.kakao_account.email,
+        default: (data) => data.email,
+    },
+    name: {
+        KAKAO: (data) => data.kakao_account.profile.nickname,
+        default: (data) => data.name,
+    },
+    id: (data) => data.id,
+    image_url: {
+        KAKAO: (data) => data.kakao_account.profile.image_url,
+        GITHUB: (data) => data.avatar_url,
+        default: (data) => data.picture,
+    },
+};
+
+const formatData = (type, provider, data) => {
+    const formatter = typeFormatters[type] && (typeFormatters[type][provider] || typeFormatters[type].default);
+    return formatter ? formatter(data) : null;
+};
+
+const createUserRecord = async (data, type, transaction) => {
+    return await User.create({
+        email: formatData('email', type, data) || null,
+        login_type: socialCode[type],
+    }, { transaction });
+};
+
+const createSocialLoginRecord = async (userId, data, type, transaction) => {
+    const externalId = formatData('id', type, data);
+    return await SocialLogin.create({
+        user_id : userId,
+        social_code: socialCode[type],
+        external_id: typeof(externalId) === "number"? externalId.toString() : externalId,
+    }, { transaction });
+}
+
+const createPreferenceRecord = async (userId, transaction) => {
+    return await Preference.create({
+        user_id: userId
+    }, { transaction });
+};
+
+const createProfileRecord = async (userId, data, type, transaction) => {
+    const name = formatData('name', type, data);
+    return await Profile.create({
+        user_id: userId,
+        nickname: `${type}${name}`,
+        image_url: formatData('image_url', type, data),
+    }, { transaction });
+};
+
 export const socialLoginRepository = {
     findBySocialId: async (socialId) => {
         return await SocialLogin.findOne({ where: { external_id : socialId }});
     },
-    createSocialUser: async (data) => {
+    createSocialUser: async (data, type) => { // userRepository의 createUser 참조하여 만들기
         const transaction = await sequelize.transaction();
         try {
-            const user = await User.create({
-                email: data.email || null,
-                login_type: data.code
-            }, { transaction });
-            await SocialLogin.create({
-                user_id : user.user_id,
-                social_code: data.code,
-                external_id: typeof(data.id) === "number" ? data.id.toString() : data.id
-            }, { transaction });
-            await Preference.create({
-                user_id: user.user_id
-            }, { transaction });
-            await Profile.create({
-                user_id: user.user_id,
-                nickname: `${data.type}${data.name}`,
-                image_url: data.image_url || null
-            }, { transaction });
-
+            const user = await createUserRecord(data, type, transaction);
+            await Promise.all([
+                createSocialLoginRecord(user.user_id, data, type, transaction),
+                createPreferenceRecord(user.user_id, transaction),
+                createProfileRecord(user.user_id, data, type, transaction)
+            ]);
             await transaction.commit();
             return user;
         } catch (error) {
             await transaction.rollback();
-            return { error };
+            throw error;
         }
-    }
+    },
+    // createSocialUser: async (data) => {
+    //     const transaction = await sequelize.transaction();
+    //     try {
+    //         const user = await User.create({
+    //             email: data.email || null,
+    //             login_type: data.code
+    //         }, { transaction });
+    //         await SocialLogin.create({
+    //             user_id : user.user_id,
+    //             social_code: data.code,
+    //             external_id: typeof(data.id) === "number" ? data.id.toString() : data.id
+    //         }, { transaction });
+    //         await Preference.create({
+    //             user_id: user.user_id
+    //         }, { transaction });
+    //         await Profile.create({
+    //             user_id: user.user_id,
+    //             nickname: `${data.type}${data.name}`,
+    //             image_url: data.image_url || null
+    //         }, { transaction });
+    //
+    //         await transaction.commit();
+    //         return user;
+    //     } catch (error) {
+    //         await transaction.rollback();
+    //         return { error };
+    //     }
+    // },
+    // createSocialUserByCode: async (data) => {
+    //     const transaction = await sequelize.transaction();
+    // }
 }
--- src/service/social.login.service.js
+++ src/service/social.login.service.js
@@ -1,5 +1,5 @@
-import { socialLoginRepository } from '@/repository';
-import { createToken } from '@/utils/index'
+import { profileRepository, socialLoginRepository, userRepository } from '@/repository';
+import { createToken } from '@/utils'
 import axios from 'axios';
 
 const socialCode = {
@@ -21,7 +21,63 @@ const kakaoOptions = {
     clientSecret: process.env.KAKAO_SECRET,
 }
 
+const Options = [null, { // 1번 index : KAKAO
+    clientID: kakaoOptions.clientID,
+    clientSecret: kakaoOptions.clientSecret,
+    requestToken: 'https://kauth.kakao.com/oauth/token',
+    profile: 'https://kapi.kakao.com/v2/user/me',
+}, { // 2번 index : GITHUB
+    clientID: githubOptions.clientID,
+    clientSecret: githubOptions.clientSecret,
+    requestToken: 'https://github.com/login/oauth/access_token',
+    profile: 'https://api.github.com/user',
+}, { // 3번 index : GOOGLE
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    requestToken: 'https://oauth2.googleapis.com/token',
+    profile: 'https://www.googleapis.com/userinfo/v2/me',
+}];
+
 export const socialLoginService = {
+    login: async (type, code, uri) => { // type에 .toUpperCase 사용해서 전달하기
+        let profile, email;
+        try {
+            const socialToken = await axios.post(Options[type].requestToken, null, {
+                params: {
+                    code,
+                    client_id: Options[type].clientID,
+                    client_secret: Options[type].clientSecret,
+                    redirect_uri: uri,
+                    ...(type === "GITHUB" ? {} : { grant_type: 'authorization_code' })
+                }
+            });
+            const socialProfile = await axios.get(Options[type].profile, {
+                headers: {
+                    'Authorization': `Bearer ${type === "GITHUB" ?
+                        new URLSearchParams(socialToken.data).get('access_token') : socialToken.data.access_token}`
+                }
+            });
+            const user = await socialLoginRepository.findBySocialId(socialProfile.data.id);
+            profile = await profileRepository.findUserInformationById(user.user_id);
+            email = userRepository.findEmailByUserId(user.user_id);
+            if (!user) {
+                const newUser = await socialLoginRepository.createSocialUser(socialProfile.data, type);
+                profile = await profileRepository.findUserInformationById(newUser.user_id);
+                email = newUser.dataValues.email;
+            }
+            const userToken = await createToken(profile.user_id);
+            return {
+                token: userToken,
+                profile : {
+                    nickname: profile.nickname,
+                    image_url: profile.image_url,
+                    email
+                }
+            }
+        } catch (error) {
+            return { message: error.message }
+        }
+    },
     kakaoLoginService: async (code, uri) => {
         try {
             const token = await axios.post('https://kauth.kakao.com/oauth/token', null, {
