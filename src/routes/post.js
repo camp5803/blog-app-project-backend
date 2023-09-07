5070e51f72d76ae67d52964a5550e8e11ad469a3
jjoonior <jjoonior97@gmail.com>
2023-09-07 21:12:40 +0900

    [Fix] post 라우터 메소드 포맷 수정

--- src/routes/post.js
+++ src/routes/post.js
@@ -13,15 +13,17 @@ import {
 const router = express.Router();
 import { isAuthenticated, isAuthorized } from "@/middleware";
 
-router.get('/post/all/:sort/:id').get(getPostsByPage);
-router.get('/post/detail/:id').get(getByPostDetail);
+// 토큰 검증 필요 x
+router.get('/post/all/:sort/:id', getPostsByPage);
+router.get('/post/detail/:id', getByPostDetail);
 
+// 토큰 검증 필요 o
 router.use(isAuthenticated);
 router.post('/post', createPost);
 router.get('/post/:id/verification', verifyUser);
-router.post('/post/bookmark').post(toggleBookmark);
-router.post('/post/like').post(toggleLike);
+router.post('/post/bookmark', toggleBookmark);
+router.post('/post/like', toggleLike);
 router.patch('/post/:id', updatePost);
-router.delete('/post/:id').delete(deletePost);
+router.delete('/post/:id', deletePost);
 
 export default router;
