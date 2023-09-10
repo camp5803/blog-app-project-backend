94d72dd98eee7892f8f7e334ecac54e14f3cec41
jjoonior <jjoonior97@gmail.com>
2023-09-10 19:04:10 +0900

    [fix] 오타 수정

--- src/routes/post.js
+++ src/routes/post.js
@@ -12,7 +12,7 @@ router.get('/post/:id/verification', postController.verifyUser);
 // 토큰 검증 필요 o
 router.use(isAuthenticated);
 router.post('/post', postController.createPost);
-router.post('/post/bookmark', postController.toggleBookm정ark);
+router.post('/post/bookmark', postController.toggleBookmark);
 router.post('/post/like', postController.toggleLike);
 router.patch('/post/:id', postController.updatePost);
 router.delete('/post/:id', postController.deletePost);
