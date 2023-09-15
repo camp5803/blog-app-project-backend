7c22031f14f5a04e1a38cbae42fef3ff7f8f61fa
Jiyong Jung <hickerd@sch.ac.kr>
2023-09-16 04:15:33 +0900

    [Feat] 이웃 서비스 인덱스 등록

--- src/service/index.js
+++ src/service/index.js
@@ -3,3 +3,4 @@ export { authService } from '@/service/auth.service';
 export { socialLoginService } from '@/service/social.login.service';
 export { preferenceService } from '@/service/preference.service';
 export { keywordService } from '@/service/keyword.service';
+export { neighborService } from '@/service/neighbor.service';
