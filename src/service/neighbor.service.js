98663f3fecde645eb1f58c11b760e95b81a9b10a
Jiyong Jung <hickerd@sch.ac.kr>
2023-09-16 03:58:09 +0900

    [Fix] 이웃 쿼리 수정 및 팔로우 언팔로우 기능 추가

--- src/service/neighbor.service.js
+++ src/service/neighbor.service.js
@@ -1,19 +1,18 @@
-import { neighborRepository } from '@/repository';
+import { neighborRepository, userRepository, blockRepository } from '@/repository';
+import { customError } from '@/common/error';
+import { StatusCodes } from 'http-status-codes';
 
-export const userService = {
+export const neighborService = {
     getFollowers: async (userId) => {
         try {
             const data = await neighborRepository.findFollowersByUserId(userId);
-            const followers = [];
-
-            data.forEach(follower => {
-                followers.push({
+            const followers = data.map(follower => {
+                return {
                     userId: follower.userId,
-                    nickname: follower.Profile.nickname,
-                    imageUrl: follower.Profile.imageUrl
-                });
+                    nickname: follower.nickname,
+                    imageUrl: follower.imageUrl
+                }
             });
-            
             return followers;
         } catch (error) {
             throw customError(StatusCodes.INTERNAL_SERVER_ERROR, error.message);
@@ -22,19 +21,65 @@ export const userService = {
     getFollowings: async (userId) => {
         try {
             const data = await neighborRepository.findFollowingsByUserId(userId);
-            const followings = [];
-
-            data.forEach(follower => {
-                followings.push({
-                    userId: follower.userId,
-                    nickname: follower.Profile.nickname,
-                    imageUrl: follower.Profile.imageUrl
-                });
+            const followings = data.map(following => {
+                return {
+                    userId: following.userId,
+                    nickname: following.nickname,
+                    imageUrl: following.imageUrl
+                }
             });
-            
             return followings;
         } catch (error) {
             throw customError(StatusCodes.INTERNAL_SERVER_ERROR, error.message);
         }
+    },
+    follow: async (userId, targetUserId) => {
+        try {
+            const user = await userRepository.findByUserId(targetUserId);
+            const isFollowing = await neighborRepository.isFollowing(userId, targetUserId);
+            if (!user || isFollowing) {
+                if (!user) {
+                    throw customError(StatusCodes.PRECONDITION_REQUIRED, "The user you are trying to follow does not exist.");
+                }
+                throw customError(StatusCodes.CONFLICT, "You're already a follower.");
+            }
+            return await neighborRepository.follow(userId, targetUserId);
+        } catch (error) {
+            throw customError(StatusCodes.INTERNAL_SERVER_ERROR, error.message);
+        }
+    },
+    unfollow: async (userId, targetUserId) => {
+        try {
+            const user = await userRepository.findByUserId(targetUserId);
+            const isFollowing = await neighborRepository.isFollowing(userId, targetUserId);
+            if (!user || !isFollowing) {
+                if (!user) {
+                    throw customError(StatusCodes.PRECONDITION_REQUIRED, "The user you are trying to unfollow does not exist.");
+                }
+                throw customError(StatusCodes.CONFLICT, "You are not already following the user.");
+            }
+            return await neighborRepository.unfollow(userId, targetUserId);
+        } catch (error) {
+            throw customError(StatusCodes.INTERNAL_SERVER_ERROR, error.message);
+        }
+    },
+    blockUser: async (userId, blockUserId) => {
+        try {
+            const user = await userRepository.findByUserId(blockUserId);
+            const isBlocked = await blockRepository.isBlocked(userId, blockUserId);
+            if (!user || isBlocked) {
+                if (!user) {
+                    throw customError(StatusCodes.PRECONDITION_REQUIRED, "The user you are trying to block does not exist.");
+                }
+                throw customError(StatusCodes.CONFLICT, "Already blocked user.");
+            }
+            const data = await blockRepository.block(userId, blockUserId);
+            return data.dataValues.blockUserId;
+        } catch (error) {
+            throw customError(error.status || StatusCodes.INTERNAL_SERVER_ERROR, error.message);
+        }
+    },
+    getBlockUsers: async () => {
+
     }
 }
